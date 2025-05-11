3f192a46fc28a256e2162bfab29a364ea55ccb63
Alan McNatty <alan@encounter.nz>
2025-05-11 22:57:21 +1200

    tilt-hydrometer first cut - still beta testing 3d model but getting a commit up

---
+++ hydrometer/micropython/ports/rp2/modules/config.py
@@ -0,0 +1,20 @@
+wifi_ssid = 'luna'
+wifi_password = 'marinka4me'
+
+http_api = 'http://doom.box'
+time_to_sleep = 60 # seconds
+
+mqtt_username = b'doom_mqtt'
+mqtt_password = b"It's a freak Sc3ne"
+
+mqtt_server_mine = b'wipeout.box'
+mqtt_port_mine = 1883
+mqtt_ssl_mine = False
+
+mqtt_server_hive = b'52cc92543cfa45289e0a34ff41287ce3.s1.eu.hivemq.cloud'
+mqtt_port_hive = 8883
+mqtt_ssl_hive = True
+
+mqtt_server = mqtt_server_mine
+mqtt_port = mqtt_port_mine
+mqtt_ssl = mqtt_ssl_mine
